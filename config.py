f5753abc43fd28625e2f6c7d6639cc6fc8ba2891
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-06 18:22:31 +0300

    changes with a new repo

---
+++ config.py
@@ -0,0 +1,32 @@
+
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADS_DEFAULT_DEST ='app/static'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 588
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/pitch'
+
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://george:PTEXPTEX61@localhost/pitch'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='1234'
+SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://george:PTEXPTEX61@localhost/pitch'
+export MAIL_USERNAME='gnjoro077@gmail.com'
+export MAIL_PASSWORD='ptexptex'
+
+
+python3.6 manage.py server
