b0d7599b975d3a81eef31f331fc08e6a2c33f274
GeorgeKinuthia77 <gnjoro077@gmail.com>
2019-08-07 11:23:34 +0300

    deployment

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
 
 
 class ProdConfig(Config):
-        SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 
